0c4a38e60329fa57e8e3b8cc82c2e29e1d48f43f
Ebou Jobe <ebou.jobe@mail.mcgill.ca>
2018-04-26 18:55:39 -0400

    refactor: I refactored for more code redeability

--- code/mobile/src/modules/user/api/state.js
+++ code/mobile/src/modules/user/api/state.js
@@ -17,7 +17,7 @@ export default (state = userInitialState, action) => {
       return {
         ...state,
         isAuthenticated: !isEmpty(action.user),
-        details: action.user,
+        details: action.user
       }
 
     case LOGIN_REQUEST:
@@ -35,13 +35,7 @@ export default (state = userInitialState, action) => {
       }
 
     case LOGOUT:
-      return {
-        ...state,
-        error: null,
-        isLoading: false,
-        isAuthenticated: false,
-        details: null
-      }
+      return userInitialState
 
     default:
       return state
